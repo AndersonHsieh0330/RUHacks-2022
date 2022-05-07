3f7743c410437152fb4b3a3b7d0a4e881350daf4
Anderson Hsieh <andy13579246810@gmail.com>
2022-05-07 19:26:39 -0400

    add Access-Control-Allow-Origin header to api endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,7 @@ router.get('/',async(req,res,next)=>{
     
     const recipes = await Recipe.find();
     const jsonContent = JSON.stringify(recipes);
+    res.header("Access-Control-Allow-Origin", "*");
 
     res.send(jsonContent);
 })
@@ -14,6 +15,7 @@ router.get('/:id', async(req, res, next) => {
     const new_id = new mongoose.Types.ObjectId(req.params.id)
     const recipe = await Recipe.findById(new_id);
     const jsonContent = JSON.stringify(recipe);
+    res.header("Access-Control-Allow-Origin", "*");
 
     res.send(jsonContent);
 });
@@ -27,6 +29,8 @@ router.post('/',async(req,res,next)=>{
     const id = {
         _id:recipe._id.toString()
     }
+    res.header("Access-Control-Allow-Origin", "*");
+
     res.send(id);
 })
 
