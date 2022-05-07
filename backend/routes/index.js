62264d5610c3f70d4e461a9346270c1453af0833
SahilHakimiUofT <71340008+SahilHakimiUofT@users.noreply.github.com>
2022-05-07 16:44:14 -0400

    backend changes, added the needed endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,35 @@
 const router = require('express').Router();
+const connection = require('../config/database');
+const Recipe = connection.models.Recipe;
+const mongoose = require('mongoose');
 
-router.get('/', (req, res, next) => {
-    res.send('Successful test request');
+router.get('/',async(req,res,next)=>{
+    
+    const recipes = await Recipe.find();
+    const jsonContent = JSON.stringify(recipes);
+
+    res.send(jsonContent);
+})
+router.get('/:id', async(req, res, next) => {
+    const new_id = new mongoose.Types.ObjectId(req.params.id)
+    const recipe = await Recipe.findById(new_id);
+    const jsonContent = JSON.stringify(recipe);
+
+    res.send(jsonContent);
 });
+
+router.post('/',async(req,res,next)=>{
+
+    const cooking_methods_request = req.body.cooking_methods;
+    const recipe = await Recipe.create({
+        cooking_methods:req.body.cooking_methods
+    })
+    const id = {
+        _id:recipe._id.toString()
+    }
+    res.send(id);
+})
+
 
 
 
