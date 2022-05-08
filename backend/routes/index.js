b1f2db43d97007117aca9439a55cd4b9eb367b01
Anderson Hsieh <andy13579246810@gmail.com>
2022-05-08 03:47:41 -0400

    implement server side request to open screen

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -69,31 +69,28 @@ router.post('/generateQRCode', async(req,res,next)=>{
         Authorization: 'Bearer eyJraWQiOiJ4ZFFwRkoxVDRRWnJMWXJYVHR4VVA5SVUxMGh3M240K0FTS1hWcWxaZkNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2OTYxNGM1MC0zOGI0LTQ3M2UtYWVhMi02MGE0NjYyZTc4NzQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9Bd2ZOUzA4bHMiLCJjbGllbnRfaWQiOiI2MXJrM3JhOW9sbjJhMDhlMm9oNG12dnE3ayIsIm9yaWdpbl9qdGkiOiIyZWEzZjFlNS03MTdlLTQ5YTctOTY1My03MzVhNWRhZWE0MmUiLCJldmVudF9pZCI6Ijg2MDdkZWUxLThiZWQtNGYyMC1iZTg2LTI2MTUwYjA5NTU1MiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTE5NTk3MzQsImV4cCI6MTY1MjAwMjkzNCwiaWF0IjoxNjUxOTU5NzM0LCJqdGkiOiIxNzdhODI2YS04ZTkwLTQyYTYtYjY4Zi0wYTc0ZjAwNDdmZWEiLCJ1c2VybmFtZSI6ImZ5YmJ4ZmRhejl0c2h2Y2tyMiJ9.fiWwBFgwkGscgJ-OW6f9SVKOSNhWt3TyOq-ppdp8RkWssNbwRaRNCaAcqxnVJt81oXgpdc8Tji3NMpqPOMNCszLf62jDQOOalb-8R5J1IaOpRkf2hzYSkt3MsrVjSe2pjLNmgZbOx3XyCBaREJj23YQAYEQLx_2xiAYFS_q4C3sgw4NqSjVeWFWqQhrkn9U7XgQTBFfS6HEVW1xw9jLjQRcOcf9hApeATaN-PGzD07IvoiCTo_VMXavTsQ-aSe1Nb2vKA_u6rxXoqBHfBUbO6-V5B4_o8hpPh1HW-urptK6bG6R5ta17tyhH84N5yxP7K-iEXOcVNs8smQN300h24g'
       }
 })
-.then(res => {
-    console.log(res.data.asset.assetId);
-    const assetID = res.data.asset.assetId;
+.then(response => {
+    const assetID = response.data.asset.assetId;
+    console.log(assetID);
     //https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/assets/4d0a3a5d-5f78-4da8-90e2-063dc961b6ea/qrcodes
-    const url = "https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/assets/"+assetID+"/qrcodes";
+    const url = "https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/assets/"+assetID+"/qrcodes?dataUrl=true";
     axios({
         method: "GET",
         url:url,
-        data:{
-            "dataUrl" : true
-       },
         headers: {
             Accept: 'application/json',
             Authorization: 'Bearer eyJraWQiOiJ4ZFFwRkoxVDRRWnJMWXJYVHR4VVA5SVUxMGh3M240K0FTS1hWcWxaZkNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2OTYxNGM1MC0zOGI0LTQ3M2UtYWVhMi02MGE0NjYyZTc4NzQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9Bd2ZOUzA4bHMiLCJjbGllbnRfaWQiOiI2MXJrM3JhOW9sbjJhMDhlMm9oNG12dnE3ayIsIm9yaWdpbl9qdGkiOiIyZWEzZjFlNS03MTdlLTQ5YTctOTY1My03MzVhNWRhZWE0MmUiLCJldmVudF9pZCI6Ijg2MDdkZWUxLThiZWQtNGYyMC1iZTg2LTI2MTUwYjA5NTU1MiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTE5NTk3MzQsImV4cCI6MTY1MjAwMjkzNCwiaWF0IjoxNjUxOTU5NzM0LCJqdGkiOiIxNzdhODI2YS04ZTkwLTQyYTYtYjY4Zi0wYTc0ZjAwNDdmZWEiLCJ1c2VybmFtZSI6ImZ5YmJ4ZmRhejl0c2h2Y2tyMiJ9.fiWwBFgwkGscgJ-OW6f9SVKOSNhWt3TyOq-ppdp8RkWssNbwRaRNCaAcqxnVJt81oXgpdc8Tji3NMpqPOMNCszLf62jDQOOalb-8R5J1IaOpRkf2hzYSkt3MsrVjSe2pjLNmgZbOx3XyCBaREJj23YQAYEQLx_2xiAYFS_q4C3sgw4NqSjVeWFWqQhrkn9U7XgQTBFfS6HEVW1xw9jLjQRcOcf9hApeATaN-PGzD07IvoiCTo_VMXavTsQ-aSe1Nb2vKA_u6rxXoqBHfBUbO6-V5B4_o8hpPh1HW-urptK6bG6R5ta17tyhH84N5yxP7K-iEXOcVNs8smQN300h24g'
           }
     })
-    .then(res => {
-        console.log(res.data.qrCodes[0].image.data);
+    .then(response => {
+        const dataURL = response.data.qrCodes[0].image.data;
+        console.log(dataURL);
+        res.send(dataURL);
     })
     .catch(error => {
     console.log(error);
 })
-
 })
-
 })
 
 module.exports = router;
