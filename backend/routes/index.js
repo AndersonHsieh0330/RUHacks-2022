fcb701255189928787b172d5c614846203de5387
Anderson Hsieh <andy13579246810@gmail.com>
2022-05-08 03:15:00 -0400

    complete first OS api call

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,8 @@ const connection = require('../config/database');
 const Recipe = connection.models.Recipe;
 const mongoose = require('mongoose');
 const axios = require('axios');
-// const fetch = require('node-fetch');
+require('dotenv').config();
+const localURL = process.env.LOCALURL;
 
 router.get('/',async(req,res,next)=>{
     const recipes = await Recipe.find();
@@ -50,14 +51,16 @@ router.post('/generateQRCode', async(req,res,next)=>{
   const OS_API_SECRET = "i0rMx21izP3OGslQ5k8iwSYd";
   const OS_PROJECT_ID = "95786b59-0362-4afb-a909-33a641fc8a53";
     const currentRecipeID = req.body.currentRecipeID;
+    const recipeName = req.body.recipeName;
 
   axios({
     method: "POST",
     url:"https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/projects/95786b59-0362-4afb-a909-33a641fc8a53/assets",
     data:{
+        "name":recipeName,
     "qrCodes": [
          {
-              "intent": "http://localhost:3000/" + currentRecipeID
+              "intent": localURL + currentRecipeID
          }
     ]},
     headers: {
@@ -67,46 +70,29 @@ router.post('/generateQRCode', async(req,res,next)=>{
       }
 })
 .then(res => {
-    console.log(res);
-//     axios.post(`https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/assets/assetId/qrcodes \
-//       `, {
-//           "assetId": "032a89fc-75eb-42c9-b525-d0815df19507"
-//           ,"dataUrl" : true
-//      }
-//   ).then(res => {
-//       editQRCodeDataURL(res.qrCodes[0].image.data);
-//           console.log("imgDataURL created");
-//           console.log(res.qrCodes[0].image.data);
-//       }).catch(error => {
-//           console.log(error);
-//       })
-
-}).catch(error => {
+    console.log(res.data.asset.assetId);
+    const assetID = res.data.asset.assetId;
+    const url = "https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/assets/"+assetID+"/qrcodes";
+    axios({
+        method: "GET",
+        url:url,
+        data:{
+            "dataUrl" : true
+       },
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer eyJraWQiOiJ4ZFFwRkoxVDRRWnJMWXJYVHR4VVA5SVUxMGh3M240K0FTS1hWcWxaZkNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2OTYxNGM1MC0zOGI0LTQ3M2UtYWVhMi02MGE0NjYyZTc4NzQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9Bd2ZOUzA4bHMiLCJjbGllbnRfaWQiOiI2MXJrM3JhOW9sbjJhMDhlMm9oNG12dnE3ayIsIm9yaWdpbl9qdGkiOiIyZWEzZjFlNS03MTdlLTQ5YTctOTY1My03MzVhNWRhZWE0MmUiLCJldmVudF9pZCI6Ijg2MDdkZWUxLThiZWQtNGYyMC1iZTg2LTI2MTUwYjA5NTU1MiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTE5NTk3MzQsImV4cCI6MTY1MjAwMjkzNCwiaWF0IjoxNjUxOTU5NzM0LCJqdGkiOiIxNzdhODI2YS04ZTkwLTQyYTYtYjY4Zi0wYTc0ZjAwNDdmZWEiLCJ1c2VybmFtZSI6ImZ5YmJ4ZmRhejl0c2h2Y2tyMiJ9.fiWwBFgwkGscgJ-OW6f9SVKOSNhWt3TyOq-ppdp8RkWssNbwRaRNCaAcqxnVJt81oXgpdc8Tji3NMpqPOMNCszLf62jDQOOalb-8R5J1IaOpRkf2hzYSkt3MsrVjSe2pjLNmgZbOx3XyCBaREJj23YQAYEQLx_2xiAYFS_q4C3sgw4NqSjVeWFWqQhrkn9U7XgQTBFfS6HEVW1xw9jLjQRcOcf9hApeATaN-PGzD07IvoiCTo_VMXavTsQ-aSe1Nb2vKA_u6rxXoqBHfBUbO6-V5B4_o8hpPh1HW-urptK6bG6R5ta17tyhH84N5yxP7K-iEXOcVNs8smQN300h24g'
+          }
+    })
+    .then(res => {
+        console.log(res.data.qrCodes[0].image.data);
+    })
+    .catch(error => {
     console.log(error);
 })
 
-// const options = {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: 'Bearer eyJraWQiOiJ4ZFFwRkoxVDRRWnJMWXJYVHR4VVA5SVUxMGh3M240K0FTS1hWcWxaZkNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2OTYxNGM1MC0zOGI0LTQ3M2UtYWVhMi02MGE0NjYyZTc4NzQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9Bd2ZOUzA4bHMiLCJjbGllbnRfaWQiOiI2MXJrM3JhOW9sbjJhMDhlMm9oNG12dnE3ayIsIm9yaWdpbl9qdGkiOiIyZWEzZjFlNS03MTdlLTQ5YTctOTY1My03MzVhNWRhZWE0MmUiLCJldmVudF9pZCI6Ijg2MDdkZWUxLThiZWQtNGYyMC1iZTg2LTI2MTUwYjA5NTU1MiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTE5NTk3MzQsImV4cCI6MTY1MjAwMjkzNCwiaWF0IjoxNjUxOTU5NzM0LCJqdGkiOiIxNzdhODI2YS04ZTkwLTQyYTYtYjY4Zi0wYTc0ZjAwNDdmZWEiLCJ1c2VybmFtZSI6ImZ5YmJ4ZmRhejl0c2h2Y2tyMiJ9.fiWwBFgwkGscgJ-OW6f9SVKOSNhWt3TyOq-ppdp8RkWssNbwRaRNCaAcqxnVJt81oXgpdc8Tji3NMpqPOMNCszLf62jDQOOalb-8R5J1IaOpRkf2hzYSkt3MsrVjSe2pjLNmgZbOx3XyCBaREJj23YQAYEQLx_2xiAYFS_q4C3sgw4NqSjVeWFWqQhrkn9U7XgQTBFfS6HEVW1xw9jLjQRcOcf9hApeATaN-PGzD07IvoiCTo_VMXavTsQ-aSe1Nb2vKA_u6rxXoqBHfBUbO6-V5B4_o8hpPh1HW-urptK6bG6R5ta17tyhH84N5yxP7K-iEXOcVNs8smQN300h24g'
-//     }
-//   };
-  
-//   fetch('https://kbdgsb6g57.execute-api.us-east-1.amazonaws.com/prod/projects/95786b59-0362-4afb-a909-33a641fc8a53/assets', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-    
-
 })
 
-
-
-
-
-
-
+})
 
 module.exports = router;
