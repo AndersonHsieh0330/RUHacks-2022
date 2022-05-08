32aad5e963c6f5a0d6cb168ff217aef5b5742c92
Manaraa <manaraa97@hotmail.com>
2022-05-07 23:37:30 -0400

    adding dynamic recipe name

--- web/src/components/Cooking/Recipe.js
+++ web/src/components/Cooking/Recipe.js
@@ -2,15 +2,49 @@ import styles from './Recipe.module.css'
 import CookingMethodList from './CookingMethodsList';
 import Card from '../UI/Card';
 import { useState } from "react";
+
 const Recipe = props =>{
 
-    const [name, setName] = useState('Fried Chicken');
-    return(
+const [name, setName] = useState('Fried Chicken');
+const [recipe,setRecipe]=useState(null)
+const [print,setPrint]=useState(false)
+function getRecipe(val){
+setRecipe(val.target.value)
+setPrint(false)
+console.warn(val.target.value)
+}
+return(
+<div>
+<div className="App"> 
+{ print?
+<h1>{recipe}</h1> : null
+}
+<input type="text" onChange={getRecipe} placeholder="Enter your Recipe Name" 
+style={{
+height:'auto',
+width:'auto', 
+padding:'5px'
+}}/ >
+
+<button 
+className="ConfirmButton"
+title='Hello'
+style={{
+height:'30px',
+width:'auto', 
+margin:'10px'
+}}
+onClick={()=>setPrint(true)}>
+
+Create Recipe
+</button>
+</div>
 <Card className = {`${styles['recipe']}`}>
-<h1>{name}</h1>
 <CookingMethodList methods={props.methods} deleteMethod = {props.deleteMethod}/>
 </Card>
-    )
+
+</div>
+)
 }
 
 export default Recipe;
